6776f7d91ac58fbc2bc7aa4cb55390d91be23a64
gokul-080 <gokulpalanivel080@gmail.com>
2025-03-10 18:04:05 +0530

    Update digitalRoot.js

--- digitalRoot.js
+++ digitalRoot.js
@@ -1,6 +1,6 @@
 function digitalRoot(numb){
-let result = numb % 9 ? numb % 9 : numb ? 9 : 0;
-return result;
+    let result = numb % 9 ? numb % 9 : numb ? 9 : 0;
+    return result;
 }
 
 console.log(digitalRoot(4));
