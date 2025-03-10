4bd501c0ab885f19c2dbe67997fec903ba81baf0
gokul-080 <gokulpalanivel080@gmail.com>
2025-03-10 18:05:26 +0530

    Update digitalRoot.js

--- digitalRoot.js
+++ digitalRoot.js
@@ -1,3 +1,5 @@
+// Sum the given the digit untill it becomes the single digit is called as digital Root.
+
 function digitalRoot(numb){
     let result = numb % 9 ? numb % 9 : numb ? 9 : 0;
     return result;
